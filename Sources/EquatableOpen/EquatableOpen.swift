187f769dc2058b86b877c51fab8d06a7eacefcfc
omochimetaru <omochi.metaru@gmail.com>
2020-01-10 14:53:34 +0900

    vwt

--- Sources/EquatableOpen/EquatableOpen.swift
+++ Sources/EquatableOpen/EquatableOpen.swift
@@ -3,35 +3,43 @@ public protocol EquatableOpener {
     init<T: Equatable>(_ value: T)
 }
 
-public func openEquatable<EO: EquatableOpener>(_ value: Any, openerType: EO.Type) -> EO {
+public func openEquatable<EO: EquatableOpener>(_ anyValue: Any, openerType: EO.Type) -> EO {
+    var anyValue = anyValue
+    return withUnsafePointer(to: &anyValue) { (anyValueRef) in
+        return openEquatable(anyValueRef: anyValueRef, openerType: openerType)
+    }
+}
+
+private func openEquatable<EO: EquatableOpener>(anyValueRef: UnsafePointer<Any>, openerType: EO.Type) -> EO {
     let symbols = Symbols.shared
     
-    let valueType = type(of: value)
+    let openerRef = UnsafeMutablePointer<EO>.allocate(capacity: 1)
+    defer {
+        openerRef.deallocate()
+    }
+    
+    let valueType = type(of: anyValueRef.pointee)
     
     guard let witness = symbols.conformsToProtocol(type: valueType,
                                                    protocol: symbols.equatableProtocolDescriptor) else
     {
-        
-        let opener = UnsafeMutablePointer<EO>.allocate(capacity: 1)
-        // initialized in generic function
-        
+        let value = projectBoxedOpaqueExistential(existential: UnsafeRawPointer(anyValueRef),
+                                                  type: valueType)
+        symbols.openEquatableT(result: UnsafeMutableRawPointer(openerRef),
+                               value: value,
+                               openerType: openerType,
+                               T: valueType,
+                               EO: openerType,
+                               witness_EO_EquatableOpener: UnsafeRawPointer(bitPattern: 0x8)!)
         defer {
-            opener.deinitialize(count: 1)
-            opener.deallocate()
+            openerRef.deinitialize(count: 1)
         }
         
-        symbols.openEquatableT(result: UnsafeMutableRawPointer(opener,
-                                                               value: <#T##UnsafeRawPointer#>,
-                                                               openerType: <#T##Any.Type#>,
-                                                               T: <#T##Any.Type#>,
-                                                               EO: <#T##Any.Type#>,
-                                                               witness_EO_EquatableOpener: <#T##UnsafeRawPointer#>)
-        
+        return openerRef.pointee
     }
     
-    
     // 黒魔術
-    return openerType.init(value)
+    return openerType.init(anyValueRef.pointee)
 }
 
 @_silgen_name("swift_openEquatable_T")
---
+++ Sources/EquatableOpen/Metadata.swift
@@ -0,0 +1,64 @@
+internal struct RefCountedStruct {
+    public var metadataPointer: UnsafeRawPointer
+    public var counter: UnsafeRawPointer
+}
+
+internal enum ValueWitnessTable {
+    public enum Offsets {
+        public static let f0 = 0
+        public static let f1 = f0 + 8
+        public static let f2 = f1 + 8
+        public static let f3 = f2 + 8
+        public static let f4 = f3 + 8
+        public static let f5 = f4 + 8
+        public static let f6 = f5 + 8
+        public static let f7 = f6 + 8
+        public static let size = f7 + 8
+        public static let stride = size + 8
+        public static let flags = stride + 8
+    }
+    
+    public struct Flags {
+        public var value: UInt32
+        
+        public init(_ value: UInt32) {
+            self.value = value
+        }
+        
+        public var isInline: Bool {
+            (value & Self.isNonInlineBit) == 0
+        }
+        
+        public var alignment: UInt {
+            UInt(value & Self.alignmentMask)
+        }
+        
+        public static let alignmentMask: UInt32 = 0x000000FF
+        public static let isNonInlineBit: UInt32 = 0x00020000
+    }
+}
+
+// see getProjectBoxedOpaqueExistentialFunction in lib/IRGen/GenExistential.cpp
+internal func projectBoxedOpaqueExistential(existential: UnsafeRawPointer,
+                                            type typeType: Any.Type) -> UnsafeRawPointer
+{
+    let type: UnsafeRawPointer = unsafeBitCast(typeType, to: UnsafeRawPointer.self)
+    let vwtRawPtr: UnsafeRawPointer = type.assumingMemoryBound(to: UnsafeRawPointer.self).advanced(by: -1).pointee
+    let vwtPtr = vwtRawPtr.assumingMemoryBound(to: UInt8.self)
+    let flagsRawPtr: UnsafeRawPointer = UnsafeRawPointer(vwtPtr.advanced(by: ValueWitnessTable.Offsets.flags))
+    let flagsRef = flagsRawPtr.assumingMemoryBound(to: UInt32.self)
+    let flags = ValueWitnessTable.Flags(flagsRef.pointee)
+    if flags.isInline {
+        return existential
+    }
+    
+    let boxAddress: UInt = existential.assumingMemoryBound(to: UInt.self).pointee
+    
+    let alignment = flags.alignment
+    
+    //  StartOffset = ((sizeof(HeapObject) + align) & ~align)
+    let heapHeaderSize = UInt(MemoryLayout<RefCountedStruct>.size)
+    let offset = (heapHeaderSize + alignment) & ~alignment
+    
+    return UnsafeRawPointer(bitPattern: boxAddress + offset)!
+}
